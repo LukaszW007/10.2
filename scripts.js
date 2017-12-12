17146a5c5277176fb6a3b583f76c17746b4e5e05
LukaszWiszniewski <wisznu07@gmail.com>
2017-12-12 17:32:20 +0100

    Added buttons and functions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
+$(document).ready(function () {
+    console.log('DOM loaded - you can have fun');
 
-$('span').each(function(index,elem){
-    $(elem)
-});
+    //longer version of function picking even elements
+    /*$('span').each(function (index, elem) {
+        if (index % 2 == 0) {
+            $(elem).css('color', 'red');
+        }
+    });*/
+
+    //short version of function picking even elements
+    $("span:even").css('color', 'red');
+
+    var paragraphs = $('p');
+    paragraphs.each(function (index, element) {
+        var button = '<button data-tmp="' + index + '">Click</button>';
+        $(element).append(button);
+    });
+
+    $('button').click(function () {
+        alert($(this).attr("data-tmp"));
+    });
+
+}, true);
+
